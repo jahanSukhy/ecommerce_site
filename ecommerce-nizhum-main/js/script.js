665a60c704f339f70897cca2c7d08e7778daf07b
Nusrath Jahan Sukhy <146944565+jahanSukhy@users.noreply.github.com>
2024-01-01 12:30:48 +0600

    Add files via upload

---
+++ ecommerce-nizhum-main/js/script.js
@@ -0,0 +1,11 @@
+// product slider 
+$(document).ready(function() {
+    $('.autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('.autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
